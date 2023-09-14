5a7be0f46f5436454d3170b61357f1e25d49f67e
OliverCodeCode CodeCode <oliversedell@gmail.com>
2023-09-14 21:00:34 +0000

    Finished Code

--- createtask.py
+++ createtask.py
@@ -1,1 +1,16 @@
-signs = ["Pig", "Dog", "Rooster", "Monkey", "Sheep", "Horse", "Snake", "Dragon", "Rabbit", "Tiger", "Ox", "Rat"]
+signs = {"Pig": 1957, "Dog": 1958, "Rooster": 1959, "Monkey": 1960, "Sheep": 1961, "Horse": 1962, "Snake": 1963, "Dragon": 1964, "Rabbit": 1965, "Tiger": 1966, "Ox": 1967, "Rat": 1968}
+birth_year = input("What year were you born?")
+birth_year = int(birth_year)
+
+def check_animals(birth_year,animal):
+    while birth_year >= signs[animal]:
+        birth_year = birth_year - 12
+        if birth_year == signs[animal]:
+            return True   
+    return False     
+
+        
+
+for animal in signs:
+    if check_animals(birth_year, animal):
+        print(animal)
